aecd97b6841c4b8024a7eae304c927d72955d93e
Valerie Russell <vruss14@gmail.com>
2021-05-21 13:40:15 -0400

    built routes for creating, reading, and updating workout info

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,55 @@
 const router = require('express').Router();
-const Cardio = require("../models/cardio.js");
-const Resistance = require("../models/resistance.js");
+const mongojs = require("mongojs");
+const Workout = require("../models/workout.js");
 
+router.post("/workouts", ({ body }, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.status(200).json(dbWorkout);
+        console.log(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+        console.log(err)
+    });
+  });
+  
+router.get("/workouts", (req, res) => {
+    Workout.find({})
+      .sort({ date: -1 })
+      .then(dbWorkout => {
+        res.status(200).json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+        console.log(err)
+      });
+});
 
+router.get("/workouts/:id", (req, res) => {
+    Workout.findOne({ _id: mongojs.ObjectId(req.params.id) })
+      .then(dbWorkout => {
+        res.status(200).json(dbWorkout);
+        console.log(dbWorkout)
+      })
+      .catch(err => {
+        res.status(400).json(err);
+        console.log(err)
+      });
+});
+
+router.put("/workouts/:id", (req, res) => {
+    Workout.findOneAndUpdate({ _id: mongojs.ObjectId(req.params.id) },
+        { $push: { exercises: req.body} },
+        { new: true })
+      .then(dbWorkout => {
+        res.status(200).json(dbWorkout);
+        console.log(dbWorkout)
+      })
+      .catch(err => {
+        res.status(400).json(err);
+        console.log(err)
+      });
+});
 
 module.exports = router;
